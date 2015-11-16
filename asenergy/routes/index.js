3bd708000812ae959d38850ce87c2c0980d9bc8f
Ayo <ayo.obembe@gmail.com>
2015-11-16 16:15:08 +0000

    Frontpage set up, and images

--- asenergy/routes/index.js
+++ asenergy/routes/index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-// var html_pages = path.join(__dirname +'public/pages'); 
-// console.log(html_pages); 
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname+'/../public/pages/index.html'));
 });
 
-router.get('/home', function(req, res, next){
-    res.sendFile('pages/index.html');
-});
 
 module.exports = router;
